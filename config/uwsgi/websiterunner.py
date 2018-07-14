10d40dc8074cc7329f1f8964ce7dec9064b5543c
Albert Wang <git@albertyw.com>
2018-07-14 14:39:12 -0700

    Simplify syspath usage

--- config/uwsgi/websiterunner.py
+++ config/uwsgi/websiterunner.py
@@ -4,17 +4,16 @@ import sys
 import dotenv
 import monitor
 import newrelic.agent
-from syspath import git_root, get_git_root  # NOQA
+from syspath import git_root
 
-root_path = get_git_root()
-sys.path.append(os.path.join(root_path, 'app'))
-dotenv.load_dotenv(os.path.join(root_path, '.env'))
+sys.path.append(os.path.join(git_root.path, 'app'))
+dotenv.load_dotenv(os.path.join(git_root.path, '.env'))
 
 monitor.start(interval=1.0)
 
 # Set up NewRelic Agent
 if os.environ['ENV'] in ['production', 'staging']:
-    newrelic_ini = os.path.join(root_path, 'config', 'newrelic.ini')
+    newrelic_ini = os.path.join(git_root.path, 'config', 'newrelic.ini')
     newrelic.agent.initialize(newrelic_ini, os.environ['ENV'])
 
 from serve import * # noqa

1521eaaade3fc18fefe3d8575aaa7c4320ad6c97
Albert Wang <git@albertyw.com>
2018-10-08 00:46:06 -0700

    Remove python autoreload in uwsgi

--- config/uwsgi/websiterunner.py
+++ config/uwsgi/websiterunner.py
@@ -1,13 +1,10 @@
 import os
 
 import dotenv
-import monitor
 import newrelic.agent
 from syspath import git_root
 
 dotenv.load_dotenv(os.path.join(git_root.path, '.env'))
-
-monitor.start(interval=1.0)
 
 # Set up NewRelic Agent
 if os.environ['ENV'] in ['production', 'staging']:

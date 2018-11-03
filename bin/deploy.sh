ddea8a0dac5a52201ff4fc5f5cfb0b2355171aae
Albert Wang <git@albertyw.com>
2018-11-03 01:50:30 -0700

    Remove hardcoded env in deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,20 +7,22 @@ set -ex
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR"/..
 
+source .env
+
 # Update repository
 git checkout master
 git fetch -tp
 git pull
 
 # Build and start container
-docker build -t $PROJECT_NAME:production .
+docker build -t $PROJECT_NAME:$ENV .
 docker stop $PROJECT_NAME || echo
 docker container prune -f
 docker run \
     --detach \
     --restart=always \
     --publish=127.0.0.1:$INTERNAL_PORT:$INTERNAL_PORT \
-    --name=$PROJECT_NAME $PROJECT_NAME:production
+    --name=$PROJECT_NAME $PROJECT_NAME:$ENV
 
 # Cleanup docker
 docker image prune -f --filter "until=336h"

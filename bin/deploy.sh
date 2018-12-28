3a73aeb5bae1ba291d35ba747d90fbe23436b743
Albert Wang <git@albertyw.com>
2018-12-28 13:44:39 +0800

    Explicitly remove old container before starting new container

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -20,6 +20,7 @@ fi
 docker build -t $PROJECT_NAME:$ENV .
 docker stop $PROJECT_NAME || echo
 docker container prune --force --filter "until=336h"
+docker container rm $PROJECT_NAME
 docker run \
     --detach \
     --restart=always \

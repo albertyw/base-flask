b84d1be98b315c722861d76b85694e25bd741a9a
Albert Wang <git@albertyw.com>
2019-11-03 13:45:11 -0800

    Switch to npm varsnap

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -4,4 +4,12 @@ var $ = require('jquery');
 global.jQuery = $;
 global.$ = $;
 
+var varsnap = require('varsnap');
+varsnap.config = {
+  varsnap: 'true',
+  env: 'production',
+  producerToken: process.env.VARSNAP_PRODUCER_TOKEN,
+  consumerToken: process.env.VARSNAP_CONSUMER_TOKEN,
+};
+
 require('./global.js');
--- bin/minify-js.js
+++ bin/minify-js.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const browserify = require('browserify');
+require('dotenv').config();
 
 const inputFile = path.join(__dirname, '..', 'app', 'static', 'js', 'index.js');
 const outputFile = path.join(__dirname, '..', 'app', 'static', 'gen', 'bundle.min.js');
 
 browserify(inputFile, {debug: true})
+  .transform('envify')
   .transform('babelify',  {presets: ['@babel/preset-env']})
   .transform('uglifyify', {compress: true, 'keep_fnames': true, global: true})
   .bundle()

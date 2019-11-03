b5cc8a42d0c1b6dfb7b6c7dabfd387ec2c78a52a
Albert Wang <git@albertyw.com>
2019-11-03 14:45:06 -0800

    Merge bootstrap into bundle

--- bin/minify-js.js
+++ bin/minify-js.js
@@ -6,10 +6,19 @@ require('dotenv').config();
 
 const inputFile = path.join(__dirname, '..', 'app', 'static', 'js', 'index.js');
 const outputFile = path.join(__dirname, '..', 'app', 'static', 'gen', 'bundle.min.js');
+const bootstrap = path.join(__dirname, '..', 'node_modules', 'bootstrap', 'dist', 'js', 'bootstrap.min.js');
+const outputStream = fs.createWriteStream(outputFile);
 
 browserify(inputFile, {debug: true})
   .transform('envify')
   .transform('babelify',  {presets: ['@babel/preset-env']})
   .transform('uglifyify', {compress: true, 'keep_fnames': true, global: true})
   .bundle()
-  .pipe(fs.createWriteStream(outputFile));
+  .pipe(outputStream);
+
+outputStream.on('finish', () => {
+  const bootstrapStream = fs.createWriteStream(outputFile, {flags: 'a'});
+  fs.readFile(bootstrap, (err, bootstrapData) => {
+    bootstrapStream.write(bootstrapData);
+  });
+});

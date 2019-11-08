75b64c7d757b8a426f0feb625ea7f270b1bf044d
Albert Wang <git@albertyw.com>
2019-11-07 23:08:17 -0800

    Refactor third-party dependencies

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,23 +1,6 @@
-var process = require('process');
-
 var $ = require('jquery');
 global.jQuery = $;
 global.$ = $;
 
-const LogFit = require('logfit');
-const logfit = new LogFit({
-  source: process.env.LOGFIT_CLIENT_TOKEN,
-});
-logfit.report();
-
-var varsnap = require('varsnap');
-varsnap.config = {
-  varsnap: 'true',
-  env: 'production',
-  producerToken: process.env.VARSNAP_PRODUCER_TOKEN,
-  consumerToken: process.env.VARSNAP_CONSUMER_TOKEN,
-};
-
-require('./rollbar.js');
-
+require('./thirdparty.js');
 require('./global.js');

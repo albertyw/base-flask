f3ad6215cac00060eb7e959fee974c2ba4b92e86
Albert Wang <git@albertyw.com>
2019-12-28 14:15:07 +0800

    Move package installation into Dockerfile

--- bin/container_setup.sh
+++ bin/container_setup.sh
@@ -8,27 +8,6 @@ IFS=$'\n\t'
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd "$DIR/.."
 
-# Install updates and system packages
-apt-get update
-apt-get install -y --no-install-recommends \
-    build-essential \
-    locales \
-    software-properties-common \
-    gcc \
-    curl \
-    supervisor \
-    git \
-    python-minimal \
-    python-dev \
-    python3-setuptools \
-    python3.8 \
-    python3.8-dev \
-    nodejs
-apt-get clean && rm -rf /var/lib/apt/lists/*
-
-# Set locale
-sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
-
 # Set up python
 curl https://bootstrap.pypa.io/get-pip.py | python3.8
 pip3 install -r requirements.txt

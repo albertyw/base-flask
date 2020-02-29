eef2c5af6326e34fbac23cf3b80aa9bab0db0aac
Albert Wang <git@albertyw.com>
2020-02-29 00:42:17 -0800

    Refactor

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -19,11 +19,13 @@ if [ "$ENV" = "production" ]; then
     git pull
 fi
 
-# Build and start container
+# Build container and network
 docker pull "$(grep FROM Dockerfile | awk '{print $2}')"
 docker build -t "$PROJECT_NAME:$ENV" .
 docker network inspect "$PROJECT_NAME" &>/dev/null ||
     docker network create --driver bridge "$PROJECT_NAME"
+
+# Start container
 docker stop "$PROJECT_NAME" || true
 docker container rm "$PROJECT_NAME" || true
 docker run \
@@ -40,6 +42,7 @@ if [ "$ENV" = "production" ]; then
     docker container prune --force --filter "until=168h"
     docker image prune --force --filter "until=168h"
     docker volume prune --force
+    docker network prune --force
 
     # Update nginx
     sudo service nginx reload

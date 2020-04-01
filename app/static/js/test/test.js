90f20e364b7e0267035a56019b23e2b9d5c2513d
Albert Wang <git@albertyw.com>
2020-04-01 02:16:32 -0700

    Update usage of varsnap.js

--- app/static/js/test/test.js
+++ app/static/js/test/test.js
@@ -1,5 +1,12 @@
+const expect = require('chai').expect;
 const varsnap = require('varsnap');
 
 require('../thirdparty');
 
-varsnap.TestCase();
+context('Varsnap', function() {
+  this.timeout(30 * 1000);
+  it('runs with production', async function() {
+    const status = await varsnap.runTests();
+    expect(status).to.be.true;
+  });
+});

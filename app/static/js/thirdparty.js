810e2485e73e674be111d85acaa616b1a99003bb
Albert Wang <git@albertyw.com>
2020-04-18 23:13:38 -0700

    Update varsnap.js to v1.4.0

--- app/static/js/thirdparty.js
+++ app/static/js/thirdparty.js
@@ -23,12 +23,12 @@ function setupLogfit() {
 }
 
 function setupVarsnap() {
-  varsnap.config = {
+  varsnap.updateConfig({
     varsnap: 'true',
     env: process.env.ENV,
     producerToken: process.env.VARSNAP_PRODUCER_TOKEN,
     consumerToken: process.env.VARSNAP_CONSUMER_TOKEN,
-  };
+  });
 }
 
 module.exports = {

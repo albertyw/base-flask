191c881dba0ee97c05e677cb5305bce3e793e633
Albert Wang <git@albertyw.com>
2020-05-23 21:56:57 -0700

    Move segment integration into thirdparty.js

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -5,5 +5,6 @@ global.$ = $;
 var thirdParty = require('./thirdparty.js');
 thirdParty.setupRollbar();
 thirdParty.setupLogfit();
+thirdParty.setupSegment();
 thirdParty.setupVarsnap();
 require('./global.js');

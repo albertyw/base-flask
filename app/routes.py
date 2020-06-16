cba30b44bfe1ff8bf7507c1bdd01fabe2d0aa57d
Albert Wang <git@albertyw.com>
2020-06-16 01:25:40 -0700

    Add type annotations to everything

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
-
+from typing import Any
 
 handlers = Blueprint('handlers', __name__)
 
 
 @handlers.route("/")
-def index():
+def index() -> Any:
     return render_template("index.htm")
--- app/tests/tests.py
+++ app/tests/tests.py
@@ -6,25 +6,25 @@ from app import serve
 
 
 class PageCase(unittest.TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         serve.app.config['TESTING'] = True
         self.app = serve.app.test_client()
 
-    def test_index_load(self):
+    def test_index_load(self) -> None:
         self.page_test('/', b'')
 
-    def test_robots_load(self):
+    def test_robots_load(self) -> None:
         self.page_test('/robots.txt', b'')
 
-    def test_sitemap_load(self):
+    def test_sitemap_load(self) -> None:
         self.page_test('/sitemap.xml', b'')
 
-    def test_not_found(self):
+    def test_not_found(self) -> None:
         response = self.app.get('/asdf')
         self.assertEqual(response.status_code, 404)
         self.assertIn(b'Not Found', response.get_data())
 
-    def page_test(self, path, string):
+    def page_test(self, path: str, string: bytes) -> None:
         response = self.app.get(path)
         self.assertEqual(response.status_code, 200)
         self.assertIn(string, response.get_data())

5b30e67aa9de7253b6bf2747e8ee43d06def0a2d
Albert Wang <git@albertyw.com>
2021-04-18 20:19:11 +0800

    Move scaffold script into baseflask

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,7 +3,7 @@
 # This is a script that can be run on a freshly setup server (see the README
 # for more details) and bring it up to a production-ready state.  This script
 # requires sudo privileges to work and it should already be scaffolded using
-# bin/scaffold.sh
+# baseflask/scaffold.sh
 
 set -exuo pipefail
 IFS=$'\n\t'

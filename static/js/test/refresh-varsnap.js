a981e6657845a61d4f22196bb7cf9359bfc1b08f
Albert Wang <git@albertyw.com>
2021-04-18 20:13:12 +0800

    Move env var into refresh-varsnap.js

--- static/js/test/refresh-varsnap.js
+++ static/js/test/refresh-varsnap.js
@@ -1,6 +1,8 @@
 const calculateFibonacci = require('../fibonacci');
 const thirdParty = require('../thirdparty');
 
+process.env.ENV='production';
+
 function loadFibonacci() {
   for (let i=0; i<20; i++) {
     console.log(calculateFibonacci(i));

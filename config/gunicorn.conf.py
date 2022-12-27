047ee0ccc1cd78c743a8054f431c3bd297262eaf
Albert Wang <git@albertyw.com>
2022-12-27 12:11:05 -0800

    Set up access and error logging

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -7,5 +7,12 @@ bind = '0.0.0.0:5000'
 workers = multiprocessing.cpu_count() * 2 + 1
 preload_app = True
 
+accesslog = '/var/www/app/logs/gunicorn/access.log'
+errorlog = '/var/www/app/logs/gunicorn/error.log'
+access_log_format = (
+    '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" '
+    '"%(a)s"'
+)
+
 max_requests = 5000
 max_requests_jitter = 1000
